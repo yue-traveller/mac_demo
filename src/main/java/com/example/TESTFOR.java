616ca446f67a5bf27fbad6817c49923e3afdc204
yue-traveller <yueyunfei127803@gmail.com>
2020-02-04 20:43:41 +0800

    测试

---
+++ src/main/java/com/example/TESTFOR.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class TESTFOR {
+    public static void main(String[] args) {
+        String s1="runoob";
+        String s2="runoob";
+        System.out.println(s1==s2);
+    }
+
+}
